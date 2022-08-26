4e9d8bc4281c79f03873f6b27f1ec7617bd5a357
Fabian Soriano <fabo.soriano@gmail.com>
2022-08-25 20:35:21 -0700

    added fetch code per instructions

--- public/index.js
+++ public/index.js
@@ -4,6 +4,14 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    // fetched stock info from TwelveData using our API key
+    const response = await fetch(`https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1min&apikey=86a25fec29dd4e78a512e7fe44f4a551`)
+    const result = await response.json()
+        console.log(result)
+    
+    
+
+
 }
 
 main()
